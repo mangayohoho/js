e6d3b6173fd7eabdf3cf96918dfaad55f2ed234e
mangayohoho <107627529+mangayohoho@users.noreply.github.com>
2022-07-12 00:15:47 +0900

    fix

---
+++ select21.js
@@ -0,0 +1,8 @@
+window.onload = myF;
+            
+            function myF() {
+                var n000011 = document.getElementById("list");
+                
+                var option = new Option("1話", "/n00021/p00021/n00021-1");
+                n000011.appendChild(option);
+            }
