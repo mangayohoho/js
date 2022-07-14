2085ac2533280f9e90782970a10312fc552a6f05
mangayohoho <107627529+mangayohoho@users.noreply.github.com>
2022-07-14 21:53:52 +0900

    add

--- select3.js
+++ select3.js
@@ -302,4 +302,304 @@ window.onload = myF;
                 
                 var option = new Option("100話", "/n00003/p00003/n00003-100");
                 n000011.appendChild(option);
+                
+                var option = new Option("101話", "/n00003/p00003/n00003-101");
+                n000011.appendChild(option);
+                
+                var option = new Option("102話", "/n00003/p00003/n00003-102");
+                n000011.appendChild(option);
+                
+                var option = new Option("103話", "/n00003/p00003/n00003-103");
+                n000011.appendChild(option);
+                
+                var option = new Option("104話", "/n00003/p00003/n00003-104");
+                n000011.appendChild(option);
+                
+                var option = new Option("105話", "/n00003/p00003/n00003-105");
+                n000011.appendChild(option);
+                
+                var option = new Option("106話", "/n00003/p00003/n00003-106");
+                n000011.appendChild(option);
+                
+                var option = new Option("107話", "/n00003/p00003/n00003-107");
+                n000011.appendChild(option);
+                
+                var option = new Option("108話", "/n00003/p00003/n00003-108");
+                n000011.appendChild(option);
+                
+                var option = new Option("109話", "/n00003/p00003/n00003-109");
+                n000011.appendChild(option);
+                
+                var option = new Option("110話", "/n00003/p00003/n00003-110");
+                n000011.appendChild(option);
+                
+                var option = new Option("111話", "/n00003/p00003/n00003-111");
+                n000011.appendChild(option);
+                
+                var option = new Option("112話", "/n00003/p00003/n00003-112");
+                n000011.appendChild(option);
+                
+                var option = new Option("113話", "/n00003/p00003/n00003-113");
+                n000011.appendChild(option);
+                
+                var option = new Option("114話", "/n00003/p00003/n00003-114");
+                n000011.appendChild(option);
+                
+                var option = new Option("115話", "/n00003/p00003/n00003-115");
+                n000011.appendChild(option);
+                
+                var option = new Option("116話", "/n00003/p00003/n00003-116");
+                n000011.appendChild(option);
+                
+                var option = new Option("117話", "/n00003/p00003/n00003-117");
+                n000011.appendChild(option);
+                
+                var option = new Option("118話", "/n00003/p00003/n00003-118");
+                n000011.appendChild(option);
+                
+                var option = new Option("119話", "/n00003/p00003/n00003-119");
+                n000011.appendChild(option);
+                
+                var option = new Option("120話", "/n00003/p00003/n00003-120");
+                n000011.appendChild(option);
+                
+                var option = new Option("121話", "/n00003/p00003/n00003-121");
+                n000011.appendChild(option);
+                
+                var option = new Option("122話", "/n00003/p00003/n00003-122");
+                n000011.appendChild(option);
+                
+                var option = new Option("123話", "/n00003/p00003/n00003-123");
+                n000011.appendChild(option);
+                
+                var option = new Option("124話", "/n00003/p00003/n00003-124");
+                n000011.appendChild(option);
+                
+                var option = new Option("125話", "/n00003/p00003/n00003-125");
+                n000011.appendChild(option);
+                
+                var option = new Option("126話", "/n00003/p00003/n00003-126");
+                n000011.appendChild(option);
+                
+                var option = new Option("127話", "/n00003/p00003/n00003-127");
+                n000011.appendChild(option);
+                
+                var option = new Option("128話", "/n00003/p00003/n00003-128");
+                n000011.appendChild(option);
+                
+                var option = new Option("129話", "/n00003/p00003/n00003-129");
+                n000011.appendChild(option);
+                
+                var option = new Option("130話", "/n00003/p00003/n00003-130");
+                n000011.appendChild(option);
+                
+                var option = new Option("131話", "/n00003/p00003/n00003-131");
+                n000011.appendChild(option);
+                
+                var option = new Option("132話", "/n00003/p00003/n00003-132");
+                n000011.appendChild(option);
+                
+                var option = new Option("133話", "/n00003/p00003/n00003-133");
+                n000011.appendChild(option);
+                
+                var option = new Option("134話", "/n00003/p00003/n00003-134");
+                n000011.appendChild(option);
+                
+                var option = new Option("135話", "/n00003/p00003/n00003-135");
+                n000011.appendChild(option);
+                
+                var option = new Option("136話", "/n00003/p00003/n00003-136");
+                n000011.appendChild(option);
+                
+                var option = new Option("137話", "/n00003/p00003/n00003-137");
+                n000011.appendChild(option);
+                
+                var option = new Option("138話", "/n00003/p00003/n00003-138");
+                n000011.appendChild(option);
+                
+                var option = new Option("139話", "/n00003/p00003/n00003-139");
+                n000011.appendChild(option);
+                
+                var option = new Option("140話", "/n00003/p00003/n00003-140");
+                n000011.appendChild(option);
+                
+                var option = new Option("141話", "/n00003/p00003/n00003-141");
+                n000011.appendChild(option);
+                
+                var option = new Option("142話", "/n00003/p00003/n00003-142");
+                n000011.appendChild(option);
+                
+                var option = new Option("143話", "/n00003/p00003/n00003-143");
+                n000011.appendChild(option);
+                
+                var option = new Option("144話", "/n00003/p00003/n00003-144");
+                n000011.appendChild(option);
+                
+                var option = new Option("145話", "/n00003/p00003/n00003-145");
+                n000011.appendChild(option);
+                
+                var option = new Option("146話", "/n00003/p00003/n00003-146");
+                n000011.appendChild(option);
+                
+                var option = new Option("147話", "/n00003/p00003/n00003-147");
+                n000011.appendChild(option);
+                
+                var option = new Option("148話", "/n00003/p00003/n00003-148");
+                n000011.appendChild(option);
+                
+                var option = new Option("149話", "/n00003/p00003/n00003-149");
+                n000011.appendChild(option);
+                
+                var option = new Option("150話", "/n00003/p00003/n00003-150");
+                n000011.appendChild(option);
+                
+                var option = new Option("151話", "/n00003/p00003/n00003-151");
+                n000011.appendChild(option);
+                
+                var option = new Option("152話", "/n00003/p00003/n00003-152");
+                n000011.appendChild(option);
+                
+                var option = new Option("153話", "/n00003/p00003/n00003-153");
+                n000011.appendChild(option);
+                
+                var option = new Option("154話", "/n00003/p00003/n00003-154");
+                n000011.appendChild(option);
+                
+                var option = new Option("155話", "/n00003/p00003/n00003-155");
+                n000011.appendChild(option);
+                
+                var option = new Option("156話", "/n00003/p00003/n00003-156");
+                n000011.appendChild(option);
+                
+                var option = new Option("157話", "/n00003/p00003/n00003-157");
+                n000011.appendChild(option);
+                
+                var option = new Option("158話", "/n00003/p00003/n00003-158");
+                n000011.appendChild(option);
+                
+                var option = new Option("159話", "/n00003/p00003/n00003-159");
+                n000011.appendChild(option);
+                
+                var option = new Option("160話", "/n00003/p00003/n00003-160");
+                n000011.appendChild(option);
+                
+                var option = new Option("161話", "/n00003/p00003/n00003-161");
+                n000011.appendChild(option);
+                
+                var option = new Option("162話", "/n00003/p00003/n00003-162");
+                n000011.appendChild(option);
+                
+                var option = new Option("163話", "/n00003/p00003/n00003-163");
+                n000011.appendChild(option);
+                
+                var option = new Option("164話", "/n00003/p00003/n00003-164");
+                n000011.appendChild(option);
+                
+                var option = new Option("165話", "/n00003/p00003/n00003-165");
+                n000011.appendChild(option);
+                
+                var option = new Option("166話", "/n00003/p00003/n00003-166");
+                n000011.appendChild(option);
+                
+                var option = new Option("167話", "/n00003/p00003/n00003-167");
+                n000011.appendChild(option);
+                
+                var option = new Option("168話", "/n00003/p00003/n00003-168");
+                n000011.appendChild(option);
+                
+                var option = new Option("169話", "/n00003/p00003/n00003-169");
+                n000011.appendChild(option);
+                
+                var option = new Option("170話", "/n00003/p00003/n00003-170");
+                n000011.appendChild(option);
+                
+                var option = new Option("171話", "/n00003/p00003/n00003-171");
+                n000011.appendChild(option);
+                
+                var option = new Option("172話", "/n00003/p00003/n00003-172");
+                n000011.appendChild(option);
+                
+                var option = new Option("173話", "/n00003/p00003/n00003-173");
+                n000011.appendChild(option);
+                
+                var option = new Option("174話", "/n00003/p00003/n00003-174");
+                n000011.appendChild(option);
+                
+                var option = new Option("175話", "/n00003/p00003/n00003-175");
+                n000011.appendChild(option);
+                
+                var option = new Option("176話", "/n00003/p00003/n00003-176");
+                n000011.appendChild(option);
+                
+                var option = new Option("177話", "/n00003/p00003/n00003-177");
+                n000011.appendChild(option);
+                
+                var option = new Option("178話", "/n00003/p00003/n00003-178");
+                n000011.appendChild(option);
+                
+                var option = new Option("179話", "/n00003/p00003/n00003-179");
+                n000011.appendChild(option);
+                
+                var option = new Option("180話", "/n00003/p00003/n00003-180");
+                n000011.appendChild(option);
+                
+                var option = new Option("181話", "/n00003/p00003/n00003-181");
+                n000011.appendChild(option);
+                
+                var option = new Option("182話", "/n00003/p00003/n00003-182");
+                n000011.appendChild(option);
+                
+                var option = new Option("183話", "/n00003/p00003/n00003-183");
+                n000011.appendChild(option);
+                
+                var option = new Option("184話", "/n00003/p00003/n00003-184");
+                n000011.appendChild(option);
+                
+                var option = new Option("185話", "/n00003/p00003/n00003-185");
+                n000011.appendChild(option);
+                
+                var option = new Option("186話", "/n00003/p00003/n00003-186");
+                n000011.appendChild(option);
+                
+                var option = new Option("187話", "/n00003/p00003/n00003-187");
+                n000011.appendChild(option);
+                
+                var option = new Option("188話", "/n00003/p00003/n00003-188");
+                n000011.appendChild(option);
+                
+                var option = new Option("189話", "/n00003/p00003/n00003-189");
+                n000011.appendChild(option);
+                
+                var option = new Option("190話", "/n00003/p00003/n00003-190");
+                n000011.appendChild(option);
+                
+                var option = new Option("191話", "/n00003/p00003/n00003-191");
+                n000011.appendChild(option);
+                
+                var option = new Option("192話", "/n00003/p00003/n00003-192");
+                n000011.appendChild(option);
+                
+                var option = new Option("193話", "/n00003/p00003/n00003-193");
+                n000011.appendChild(option);
+                
+                var option = new Option("194話", "/n00003/p00003/n00003-194");
+                n000011.appendChild(option);
+                
+                var option = new Option("195話", "/n00003/p00003/n00003-195");
+                n000011.appendChild(option);
+                
+                var option = new Option("196話", "/n00003/p00003/n00003-196");
+                n000011.appendChild(option);
+                
+                var option = new Option("197話", "/n00003/p00003/n00003-197");
+                n000011.appendChild(option);
+                
+                var option = new Option("198話", "/n00003/p00003/n00003-198");
+                n000011.appendChild(option);
+                
+                var option = new Option("199話", "/n00003/p00003/n00003-199");
+                n000011.appendChild(option);
+                
+                var option = new Option("200話", "/n00003/p00003/n00003-200");
+                n000011.appendChild(option);
             }
